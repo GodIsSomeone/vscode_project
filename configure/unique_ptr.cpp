d90593a96924ef102cc5a7a55203e695c85f1534
GodIsSomeone <tph5559@126.com>
2019-03-26 17:09:15 +0800

    enter

---
+++ configure/unique_ptr.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<memory>
+class Mars
+{
+    public:
+        ~Mars ()
+        {
+            std::cout<<this<<"~Mars"<<std::endl;
+        }
+        void prin()
+        {
+            std::cout<<this<<",I am Mars"<<std::endl;
+        }
+};
+ 
+int main()
+{
+    {
+        auto autoMars = std::make_unique<Mars>();
+        autoMars->prin();
+        Mars *mars = new Mars;
+        std::unique_ptr<Mars> pMars(mars);
+        std::cout << "pMars->prin:          ";
+        pMars->prin();
+        std::cout << "pMars.get()->prin:    ";
+        pMars.get()->prin();
+        std::cout << "(*pMars).prin:        ";
+        (*pMars).prin();
+
+        std::cout << "pMars:" << pMars.get() << std::endl;
+        pMars.reset(new Mars);
+        std::cout << "pMars.reset.prin:      ";
+        pMars->prin();
+
+        std::unique_ptr<Mars> pMars2;
+        pMars2 = std::move(pMars);
+        std::cout << "pMars2.move.prin:      ";
+        std::cout << "pMars2:" << pMars2.get() << std::endl;
+        pMars2->prin();
+        pMars2.get_deleter();
+        std::cout << "pMars2.getdelete:" << pMars2.get() << std::endl;
+    }
+
+    system("pause");
+    return 0;
+}
