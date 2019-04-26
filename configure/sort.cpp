e3d82c594c6ae4f76142e8a78e9e3105c0294005
GodIsSomeone <tph5559@126.com>
2019-04-26 15:27:48 +0800

    sure

--- configure/sort.cpp
+++ configure/sort.cpp
@@ -23,7 +23,9 @@ void swapElement(int &x, int &y)
 }
 /*
 冒泡排序的主要思想是一趟比较，相邻元素之间进行对比，遍历一遍，可以将一个最值元素沉底。
-空间复杂度为O(n^2)
+时间复杂度为O(n^2)，最好最坏都是如此
+空间复杂度为O(1)
+比较稳定
 */
 
 void BubbleSort(int arr[], int size)
@@ -44,7 +46,9 @@ void BubbleSort(int arr[], int size)
 /*
 选择排序：与冒泡排序对比，交换次数很少，走一趟只需要一次交换  
 因为选择排序走一趟的目的是为了找到一个最值，然后进行交换。  
-时间复杂度O(n^2)  
+时间复杂度为O(n^2)，最好最坏都是如此  
+空间复杂度为O(1)  
+数组不稳定（涉及插入），链表稳定  
 */
 
 void SelectSort(int arr[], int size)
@@ -72,7 +76,9 @@ void SelectSort(int arr[], int size)
 /*
 插入排序，与冒泡和选择相比，插入排序不交换值，只是选择合适的位置。  
 维护前面的顺序数组  
-空间复杂度O(n^2)  
+时间复杂度为O(n^2)，最好最坏都是如此  
+空间复杂度为O(1)  
+链表稳定  
 */
 
 void InsertSort(int arr[], int size)
@@ -101,6 +107,7 @@ void InsertSort(int arr[], int size)
 然后对左边的数据，和右边的数据进行处理。
 时间复杂度，在最坏情况下：O(n^2)，平均时间复杂度：O(NlogN,2为底)，平均复杂度，1.3NlogN  
 空间复杂度，在最坏情况下：O(n)，平均时间复杂度：O(logN,2为底)  
+不稳定  
 */
 
 void QuickHelp(int arr[], int begin, int end)
@@ -148,6 +155,7 @@ void QuickSort(int arr[], int size)
 然后对左边的数据，和右边的数据进行处理。
 时间复杂度：平均 O(NlogN,2为底)  
 空间复杂度：O(1)  
+不稳定  
 */
 
 void create_heap(int array[], int begin, int end)
@@ -193,47 +201,103 @@ void heap_sort(int array[], int length)
 }
 
 /*
-希尔排序：大顶堆（升序，小到大），小顶堆（降序，大到小）。  
-然后对左边的数据，和右边的数据进行处理。
-时间复杂度，在最坏情况下：O(n^2)，平均时间复杂度：O(NlogN,2为底)，平均复杂度，1.3NlogN  
-空间复杂度，在最坏情况下：O(n)，平均时间复杂度：O(logN,2为底)  
+希尔排序：插入排序的一种更高效的改进版本。希尔排序是非稳定排序算法。  
+设定分组，组内进行插入排序，逐渐减少分组。 
+时间复杂度，在最坏情况下：O(n^2)，平均时间复杂度：O(N logN logN,2为底)   
+空间复杂度：O(1)  
+不稳定  
 */
 
 void shell_sort(int array[], int length)
 {
-    int h = 1;
-    while (h < length / 3)
+    /*步长设定为gap ， 每次除以2，逐渐缩小分组*/
+    for (int gap = length >> 1; gap > 0; gap >>= 1)
     {
-        h = 3 * h + 1;
-    }
-    cout<<h<<endl;
-    while (h >= 1)
-    {
-        for (int i = h; i < length; i++)
+        cout << "gap : " << gap << endl;
+        
+        /*分组，插入排序*/
+        for (int i = gap; i < length; i++)
         {
-            for (int j = i; j >= h && array[j] < array[j - h]; j -= h)
+            int temp = array[i];
+            int j = 0;
+            for (j = i - gap; j >= 0 && array[j] > temp; j -= gap)
             {
-                std::swap(array[j], array[j - h]);
+                array[j + gap] = array[j];
             }
+
+            array[j + gap] = temp;
         }
-        h = h / 3;
     }
 
     printArray(array, length);
 }
 
+/*
+归并排序：采用分治法。先分，再合，合的过程中进行比较。  
+归并排序是稳定排序，它也是一种十分高效的排序，能利用完全二叉树特性的排序一般性能都不会太差  
+归并排序的最好，最坏，平均时间复杂度均为O(nlogn)  
+空间复杂度为O(N)  
+稳定  
+*/
+void merge_sort_recursive(int *arr, int *reg, int start, int end)
+{
+    if (start >= end)
+    {
+        return;
+    }
 
+    int len = end - start, mid = (len >> 1) + start;
+    int start1 = start, end1 = mid;
+    int start2 = mid + 1, end2 = end;
+    /*左边递归*/
+    merge_sort_recursive(arr, reg, start1, end1);
+    /*右边递归*/
+    merge_sort_recursive(arr, reg, start2, end2);
 
+    /*合并*/
+    int k = start;
+    while (start1 <= end1 && start2 <= end2)
+    {
+        reg[k++] = arr[start1] < arr[start2] ? arr[start1++] : arr[start2++];
+    }
+
+    while (start1 <= end1)
+    {
+        reg[k++] = arr[start1++];
+    }
+
+    while (start2 <= end2)
+    {
+        reg[k++] = arr[start2++];
+    }
+
+    /*赋值操作*/
+    for (k = start; k <= end; k++)
+    {
+        arr[k] = reg[k];
+    }
+}
+
+void merge_sort(int *arr, const int length)
+{
+    /*避免在递归中，频繁开辟空间*/
+    int *reg = new int[length];
+    merge_sort_recursive(arr, reg, 0, length - 1);
+
+    printArray(arr, length);
+    delete[] reg;
+}
 
 int main()
 {
     int arr[10] = {23, 12, 31, 45, 32, 1, 4, 13, 45, 90};
     //BubbleSort(arr,10);
-    QuickSort(arr,10);
+    //QuickSort(arr,10);
     //heap_sort(arr,10);
     //shell_sort(arr,10);
     //SelectSort(arr,10);
     //InsertSort(arr,10);
+    merge_sort(arr, 10);
     system("pause");
     return 0;
 }
